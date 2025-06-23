89c491a56490ba991de764bb80d3b5c0c0c5dd2a
Tunn Anh <phamtuananh281103@gmail.com>
2025-06-23 10:18:31 +0700

    update

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Headers from './components/headers/Headers';
+import Main from './components/main/Main';
+import Footer from './components/footer/Footer';
+function App() {
+
+  return (
+    <>
+      <Headers />
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default App
